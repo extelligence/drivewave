45c82fe7455371f247a14e0b9039d05dda23bc4b
Takeshi Sato <master.extelligence@gmail.com>
2011-02-26 00:02:32 +0900

    Railsプロジェクトを作成。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drivewave::Application.config.secret_token = 'aad6654c298de940a175145aeb89792c9a358fdbf903a6c022fdc43245bccc78b8cc5892cb91918fc66991dab0148ee9be3df3a8f0da80ef3abb2ac89382a839'
